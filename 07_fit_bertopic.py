3c2d41cdfb2d1bb4ee40ed5413d173790cc7e705
agrgn42 <agrgn42@users.noreply.github.com>
2026-07-09 18:50:35 -0400

    Update 07_fit_bertopic.py typo

--- 07_fit_bertopic.py
+++ 07_fit_bertopic.py
@@ -24,9 +24,9 @@ def parse_args():
     )
 
     return parser.parse_args()
-    
+
 '''
-Download data from [INSERT LINK], save to preferred location, reference 
+Download data, save to preferred location, reference 
 'Comments.xml' data location in data load below.
 '''
 
