a51f57ac5989d71f82b7da778e83d8574a3c24e9
agrgn42 <agrgn42@users.noreply.github.com>
2026-07-09 11:05:03 -0400

    Update 07_fit_bertopic.py with additional info about where stackexchange data lives

--- 07_fit_bertopic.py
+++ 07_fit_bertopic.py
@@ -7,8 +7,17 @@ does both fitting and prediction, but we can split that into separate tasks.
 from sentence_transformers import SentenceTransformer
 from bertopic import BERTopic
 import pandas as pd
+from bs4 import BeautifulSoup
+import xmltodict
+import time
+from datetime import datetime
 
-with open('../data/Comments.xml', 'r') as f:
+'''
+Download data from [INSERT LINK], save to preferred location, reference 
+'Comments.xml' data location in data load below.
+'''
+
+with open('data_file', 'r') as f:
     data = f.read()
 
 xml_dict = xmltodict.parse(data)
@@ -26,6 +35,27 @@ Structure of xml file contents is {'comments': {'row': [{}, {}, {}, ...]}}. Valu
 
 This dataset contains 87,720 Astronomy Stack Exchange comments made between September 24, 2013 and 
 December 31, 2025, with 6,795 unique users represented as commenters.'''
+
+# Learn about users
+unique_userids = {}
+
+for n in xml_dict['comments']['row']:
+    for k,v in n.items():
+        if k == '@UserId':
+            if v not in unique_userids:
+                unique_userids[v]=1
+            else:
+                unique_userids[v]+=1
+
+        elif k == '@UserDisplayName':
+            if v not in unique_userids:
+                unique_userids[v]=0
+                unique_userids[v]+=1
+            else:
+                unique_userids[v]+=1
+        else:
+            continue
+
 
 
 df = pd.read_csv('stackoverflow_comments.csv')
